f88baaff74cd1ede030ab20fa91f9467c7ce0663
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-02-11 21:21:46 -0300

    primer commit

---
+++ src/main/java/ar/dev/maxisandoval/webappmaxmusic/WebappMaxmusicApplication.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.webappmaxmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappMaxmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappMaxmusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/dev/maxisandoval/webappmaxmusic/WebappMaxmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.webappmaxmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappMaxmusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
