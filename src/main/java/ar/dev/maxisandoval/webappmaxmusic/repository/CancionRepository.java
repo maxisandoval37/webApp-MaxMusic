c6dba2abd7c8dd3d179adcdfe0d40075a63f3263
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-02-23 20:33:04 -0300

    feat(repo) Agregadas clases del repositorio

---
+++ src/main/java/ar/dev/maxisandoval/webappmaxmusic/repository/CancionRepository.java
@@ -0,0 +1,9 @@
+package ar.dev.maxisandoval.webappmaxmusic.repository;
+
+import ar.dev.maxisandoval.webappmaxmusic.model.Cancion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CancionRepository extends JpaRepository<Cancion, Long> {
+}
