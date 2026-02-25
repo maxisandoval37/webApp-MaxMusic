0d16fc03f6db12101bf37ce77b7673e428e2dfe8
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-02-25 20:48:54 -0300

    feat(h2) se habilita la consola de H2 y se arranca con datos precargados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,27 @@
+INSERT INTO Artista (nacionalidad, fecha_nacimiento, email) VALUES
+                                                                ('Argentina', '1990-11-18', 'diegoalonso@maxmusic.com'),
+                                                                ('Argentina', '1990-11-18', 'ezequielgallo@maxmusic.com'),
+                                                                ('Argentina', '1990-11-18', 'eliassanti@maxmusic.com');
+
+INSERT INTO Album (titulo, genero, fecha_estreno, artista_id) VALUES
+                                                                  ('Countdown to Extinction', 'Rock', '2022-02-1', 1),
+                                                                  ('The Marshall Mathers LP', 'Rap', '2000-02-1', 2),
+                                                                  ('Ameri', 'Trap', '2022-02-1', 3);
+
+INSERT INTO Cancion (nombre, duracion, album_id) VALUES
+                                                     ('Cancion 1', 188.00, 1),
+                                                     ('Cancion 2', 128.00, 1),
+                                                     ('Cancion 3', 184.00, 1),
+                                                     ('Cancion 4', 134.00, 1);
+
+INSERT INTO Cancion (nombre, duracion, album_id) VALUES
+                                                     ('Cancion 5', 188.00, 2),
+                                                     ('Cancion 6', 128.00, 2),
+                                                     ('Cancion 7', 184.00, 2),
+                                                     ('Cancion 8', 134.00, 2);
+
+INSERT INTO Cancion (nombre, duracion, album_id) VALUES
+                                                     ('Cancion 1', 188.00, 3),
+                                                     ('Cancion 2', 128.00, 3),
+                                                     ('Cancion 3', 184.00, 3),
+                                                     ('Cancion 4', 134.00, 3);
