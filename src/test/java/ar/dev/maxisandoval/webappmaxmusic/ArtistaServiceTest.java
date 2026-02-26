9fc092865c2beafa1718b8175dd1a85d39e63f3d
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-02-25 21:12:17 -0300

    test(unittest) se crean los primeros tests con datafaker

---
+++ src/test/java/ar/dev/maxisandoval/webappmaxmusic/ArtistaServiceTest.java
@@ -0,0 +1,42 @@
+package ar.dev.maxisandoval.webappmaxmusic;
+
+import ar.dev.maxisandoval.webappmaxmusic.model.Artista;
+import ar.dev.maxisandoval.webappmaxmusic.service.ArtistaService;
+import lombok.RequiredArgsConstructor;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class ArtistaServiceTest {
+
+    private final ArtistaService artistaService;
+    private Artista artistaGuardado;
+
+    @BeforeEach
+    void setup() {
+        Faker faker = new Faker();
+
+        Artista artista = new Artista();
+        artista.setNacionalidad(faker.country().name().toLowerCase());
+        artista.setFechaNacimiento(LocalDate.now().minusYears(faker.number().numberBetween(18, 60)));
+
+        artista.setEmail(faker.internet().emailAddress());
+        artistaGuardado = artistaService.guardarArtista(artista);
+    }
+
+    @Test
+    void testGuardarArtista() {
+        assertNotNull(artistaGuardado.getId());
+        assertNotNull(artistaGuardado.getEmail());
+        assertNotNull(artistaGuardado.getFechaNacimiento());
+        assertNotNull(artistaGuardado.getNacionalidad());
+    }
+
+}
