fcef6a76677d86dba38babb9fb1f201125a4458c
maxisandoval37 <maxisandoval37@users.noreply.github.com>
2026-03-18 23:50:39 +0000

    deploy: d3aeb62c9853af45a725ae2466f6237d62678c6d

--- src/main/java/ar/dev/maxisandoval/webappmaxmusic/model/Rol.java
+++
@@ -1,7 +0,0 @@
-package ar.dev.maxisandoval.webappmaxmusic.model;
-
-public enum Rol {
-    ROL_ADMIN,
-    ROL_LECTURA,
-    ROL_ARTISTA
-}
--- src/main/java/ar/dev/maxisandoval/webappmaxmusic/repository/ArtistaRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.dev.maxisandoval.webappmaxmusic.repository;
-
-import ar.dev.maxisandoval.webappmaxmusic.model.Artista;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArtistaRepository extends JpaRepository<Artista, Long> {
-}
