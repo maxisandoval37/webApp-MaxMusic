3e4be8e91daf893bdc2936b0e9646274ba3075f6
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-05-26 19:26:15 -0300

    feat(unit tests) se agrega ArtistaServiceTest con dataFaker

--- src/test/java/ar/dev/maxisandoval/maxmusic/ArtistaServiceTest.java
+++ src/test/java/ar/dev/maxisandoval/maxmusic/ArtistaServiceTest.java
@@ -2,15 +2,15 @@ package ar.dev.maxisandoval.maxmusic;
 
 import ar.dev.maxisandoval.maxmusic.model.Artista;
 import ar.dev.maxisandoval.maxmusic.service.ArtistaService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.*;
+import net.datafaker.Faker;
 
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -19,17 +19,17 @@ class ArtistaServiceTest {
     private final ArtistaService artistaService;
     private Artista artistaGuardado;
 
-
     @BeforeEach
     void setup() {
-        //TODO DATA FAKER
+        Faker faker = new Faker();
 
         Artista artista = new Artista();
-        artista.setNacionalidad("Argentina");
-        artista.setFechaNacimiento(LocalDate.now());
-        artista.setEmail("hola@email.com");
+        artista.setNacionalidad(faker.country().name().toLowerCase());
+        artista.setFechaNacimiento(LocalDate.now().minusYears(faker.number().numberBetween(18,60)));
+        artista.setEmail(faker.internet().emailAddress());
 
         artistaGuardado = artistaService.guardarArtista(artista);
+        System.out.println(artista.getNacionalidad());
     }
 
     @Test
@@ -43,6 +43,14 @@ class ArtistaServiceTest {
 
         assertNotNull(artista);
         assertEquals(artistaGuardado.getId(), artista.getId());
+    }
+
+    @Test
+    void testObtenerArtistaPorIdInexistente() {
+        Long idInexistente = Long.MAX_VALUE;
+
+        EntityNotFoundException ex = assertThrows(EntityNotFoundException.class, () -> artistaService.obtenerArtistaPorId(idInexistente));
+        assertTrue(ex.getMessage().contains("No se encontró el artista con el id:"));
     }
 
 }
