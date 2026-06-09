49d1f287bc318bddde7cff48b57df02e6d51238b
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-06-09 20:08:47 -0300

    feat(filterChain) se agrega la clase SecurityConfig

---
+++ src/main/java/ar/dev/maxisandoval/maxmusic/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package ar.dev.maxisandoval.maxmusic.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.boot.security.autoconfigure.web.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(crsf -> crsf.ignoringRequestMatchers(toH2Console()))
+                .userDetailsService(userDetailsService)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/","/registro", "/1.png").permitAll()
+                        .requestMatchers(toH2Console()).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer
+                        .loginPage("/login").permitAll()
+                        .defaultSuccessUrl("/albumes"))
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                )
+                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        return http.build();
+    }
+
+}
