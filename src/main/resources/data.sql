804bf3d2aa799897e283436004fda2f4bec6453c
Maximiliano Sandoval <maxisandoval98@gmail.com>
2026-06-09 20:23:52 -0300

    feat(seguridad) se agregan usuarios de prueba y se actualizan las vistas (headers)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,3 +26,12 @@ INSERT INTO Cancion (nombre, duracion, album_id) VALUES
                                                      ( 'Cancion 10', 199.00, 4),
                                                      ( 'Cancion 11', 299.00, 4),
                                                      ( 'Cancion 12', 399.00, 4);
+
+--contraseña: admin
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido) VALUES ('admin','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_ADMIN', 'José', 'Pérez');
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido) VALUES ('lectura1','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_LECTURA', 'Martin', 'Gomez');
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido) VALUES ('lectura2','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_LECTURA', 'Juan', 'Villalba');
+
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido, artista_id) VALUES ('artista_damian','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_ARTISTA', 'damian', 'r', 1);
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido, artista_id) VALUES ('artista_caro','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_ARTISTA', 'carobf', 'bf', 2);
+INSERT INTO usuario (username, contrasena, rol, nombre, apellido, artista_id) VALUES ('artista_nico','{bcrypt}$2a$10$rJSJrFqKN2trDGzJ0AS60uBKXwJHw9b74WU5Xavfs7j/1CoWQs0fm', 'ROL_ARTISTA', 'nico', 'n', 3);
